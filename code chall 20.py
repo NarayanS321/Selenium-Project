04c1972a9415a687c703ee2ba1f15cc36597cfbe
NarayanS321 <sahoo.narayan321@gmail.com>
2020-05-08 14:57:54 +0530

    updated fb project

--- code chall 20.py
+++ code chall 20.py
@@ -1,8 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 
-driver = webdriver.Chrome(executable_path="D:\\Chrome webdriver\\chromedriver.exe")
+import time
+
+class Facebook:
+
+    def log_in_fb_project(self):
+        driver = webdriver.Chrome(executable_path="D:\\Chrome webdriver\\chromedriver.exe")
 
 
-driver.get("https://www.facebook.com/")
-email_log = driver.find_element_by_xpath("//*[@id='email']")
+        driver.get("https://www.facebook.com/")
+        driver.find_element_by_xpath("//*[@id='email']").send_keys("muna.dhana@gmail.com")
+        driver.find_element_by_xpath("//*[@id='pass']").send_keys("bonzer@7")
+        time.sleep(20)
+        driver.find_element_by_xpath("//*[@id='u_0_b']").click()
+
+        time.sleep(40)
+        driver.find_element_by_xpath("//*[@id='js_4m']/div[1]/div/div[1]/div[1]/div[2]/div/div/div/div/div/div/div["
+                                     "2]/div").send_keys("Hi how r u")
+        driver.find_element_by_xpath("//*[@id='js_4f']/div[2]/div[3]/div[2]/button").click()
+
+
+fb = Facebook()
+fb.log_in_fb_project()
+
